9285db14bfabea1cfd6ace034f18e1b769b00855
FIT HaUI <fit@QuangDM-UbuntuAI.localhost>
2024-05-02 18:49:27 +0700

    update training all bill, process data

--- tool/train_keypts_ultra.py
+++ tool/train_keypts_ultra.py
@@ -1,8 +1,8 @@
 from ultralytics import YOLO
 
-model = YOLO('yolov8m-pose.pt')  # load a pretrained model (recommended for training)
+model = YOLO('/home/fit/optivisionlab/scan-bill/yolov8m-pose.pt')  # load a pretrained model (recommended for training)
 
 # Train the model
-results = model.train(data='/home/fit/optivisionlab/scan-bill/bill_printed/vib_ver2/image_warped/data.yaml', epochs=100, imgsz=680, device='cpu', 
-                      optimizer='SGD', lr0=1e-4, batch=8, cos_lr=True, pretrained=True, project="runs/bill_printed/money/yolov8m_pose", 
-                      close_mosaic=10, degrees= 90, pose = 12, shear=0.3, scale=0.2)
+results = model.train(data='/home/fit/optivisionlab/scan-bill/dataset/bill_keypoint_landmarks/data.yaml', epochs=200, imgsz=680, device='cpu', 
+                      optimizer='AdamW', lr0=1e-4, batch=8, cos_lr=True, pretrained=True, project="runs/bill_printed/bill_all/yolov8m_pose", 
+                      close_mosaic=10, degrees= 90)
